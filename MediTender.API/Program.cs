95ff64aa15484c9a96bd3104a230b7ad10946bee
sohaila esmat <sohailabs1410@gmail.com>
2026-07-24 21:17:55 +0300

    Configure Qdrant and OpenAI without secrets

--- MediTender.API/Program.cs
+++ MediTender.API/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using MediTender.API.Data;
 using MediTender.API.Services; 
-
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Connectors.Qdrant;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,19 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IPdfParsingService, PdfParsingService>();
 builder.Services.AddScoped<ITextChunkingService, TextChunkingService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var openAiApiKey = builder.Configuration["OpenAI:ApiKey"];
+var qdrantEndpoint = builder.Configuration["Qdrant:Endpoint"];
+var qdrantApiKey = builder.Configuration["Qdrant:ApiKey"];
+
+var qdrantMemoryStore = new QdrantMemoryStore(qdrantEndpoint, 1536, apiKey: qdrantApiKey);
+
+var semanticTextMemory = new MemoryBuilder()
+    .WithOpenAITextEmbeddingGeneration("text-embedding-3-small", openAiApiKey)
+    .WithMemoryStore(qdrantMemoryStore)
+    .Build();
+
+builder.Services.AddSingleton<ISemanticTextMemory>(semanticTextMemory);
 
 var app = builder.Build();
 
