384bf5c3172392d7d5a4fa0ded31f32d01c80369
sohaila esmat <sohailabs1410@gmail.com>
2026-07-24 21:37:52 +0300

    program.cs libraries error solved

--- MediTender.API/Program.cs
+++ MediTender.API/Program.cs
@@ -1,30 +1,38 @@
+#pragma warning disable SKEXP0001
+#pragma warning disable CS0618
+#pragma warning disable CS8604
+
 using Microsoft.EntityFrameworkCore;
 using MediTender.API.Data;
-using MediTender.API.Services; 
-using Microsoft.SemanticKernel.Memory;
-using Microsoft.SemanticKernel.Connectors.Qdrant;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
+using MediTender.API.Services;
+using Qdrant.Client;
+using Microsoft.SemanticKernel;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
 builder.Services.AddScoped<IPdfParsingService, PdfParsingService>();
 builder.Services.AddScoped<ITextChunkingService, TextChunkingService>();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var openAiApiKey = builder.Configuration["OpenAI:ApiKey"];
-var qdrantEndpoint = builder.Configuration["Qdrant:Endpoint"];
-var qdrantApiKey = builder.Configuration["Qdrant:ApiKey"];
+var openAiApiKey = builder.Configuration["OpenAI:ApiKey"] ?? throw new ArgumentNullException("OpenAI:ApiKey");
+var qdrantEndpoint = builder.Configuration["Qdrant:Endpoint"] ?? throw new ArgumentNullException("Qdrant:Endpoint");
+var qdrantApiKey = builder.Configuration["Qdrant:ApiKey"] ?? throw new ArgumentNullException("Qdrant:ApiKey");
 
-var qdrantMemoryStore = new QdrantMemoryStore(qdrantEndpoint, 1536, apiKey: qdrantApiKey);
+var qdrantClient = new QdrantClient(
+    host: new Uri(qdrantEndpoint).Host,
+    https: true,
+    apiKey: qdrantApiKey
+);
+builder.Services.AddSingleton(qdrantClient);
 
-var semanticTextMemory = new MemoryBuilder()
-    .WithOpenAITextEmbeddingGeneration("text-embedding-3-small", openAiApiKey)
-    .WithMemoryStore(qdrantMemoryStore)
-    .Build();
-
-builder.Services.AddSingleton<ISemanticTextMemory>(semanticTextMemory);
+var kernelBuilder = builder.Services.AddKernel();
+kernelBuilder.AddOpenAIChatCompletion("gpt-3.5-turbo", openAiApiKey);
+kernelBuilder.AddOpenAITextEmbeddingGeneration("text-embedding-3-small", openAiApiKey);
+builder.Services.AddSingleton(kernelBuilder.Build());
 
 var app = builder.Build();
 
@@ -37,28 +45,4 @@ app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
